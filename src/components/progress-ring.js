6e1f39f097b501455bc80dac4cf69e5145022f2a
Sean Ye <seanye24@gmail.com>
2021-02-24 15:38:34 -0800

    Control progress-ring through attr and props

--- src/components/progress-ring.js
+++ src/components/progress-ring.js
@@ -1,35 +1,22 @@
-import { createElement } from '../utils';
+import { createElement, validateNumber } from '../utils';
 
+/**
+ * Custom web component representing a progress ring.
+ * @extends HTMLElement
+ * @param {number} stroke - width of circle stroke
+ * @param {number} radius - radius of circle
+ * @param {number} progress - progress of ring
+ */
 class ProgressRing extends HTMLElement {
+  static get observedAttributes() {
+    return ['radius', 'stroke', 'progress'];
+  }
+
   constructor() {
     super();
 
-    const stroke = this.getAttribute('stroke');
-    const radius = this.getAttribute('radius');
-    const normalizedRadius = radius - stroke * 2;
-    this.circumference = normalizedRadius * 2 * Math.PI;
-
     this.root = this.attachShadow({ mode: 'open' });
     this.styleElement = document.createElement('style');
-    this.styleElement.innerText = `
-      .svg {
-        overflow: hidden;
-        width: ${2 * radius}px;
-        height: ${2 * radius}px;
-      }
-
-      .circle {
-        stroke: #0095b3;
-        stroke-dasharray: ${this.circumference} ${this.circumference};
-        stroke-dashoffset: ${(1 - 0.3) * this.circumference};
-        fill: transparent;
-        stroke-width: ${stroke};
-
-        transition: 0.35s stroke-dashoffset;
-        transform: rotate(-90deg);
-        transform-origin: 50% 50%;
-      }
-    `;
 
     const svgNamespace = 'http://www.w3.org/2000/svg';
     this.svgElement = createElement(
@@ -40,17 +27,98 @@ class ProgressRing extends HTMLElement {
 
     this.circleElement = createElement(
       'circle',
-      {
-        class: 'circle',
-        r: normalizedRadius,
-        cx: radius,
-        cy: radius,
-      },
+      { class: 'circle' },
       { namespace: svgNamespace },
     );
 
+    this._radius = 0;
+    this._stroke = 0;
+    this._progress = 0;
+    this.updateComponent(this._radius, this._stroke, this._progress);
+
     this.root.append(this.styleElement, this.svgElement);
     this.svgElement.appendChild(this.circleElement);
+  }
+
+  updateComponent(radius, stroke, progress) {
+    const normalizedRadius = radius - stroke * 2;
+    const circumference = normalizedRadius * 2 * Math.PI;
+
+    this.styleElement.innerText = `
+      .svg {
+        overflow: hidden;
+        width: ${2 * radius}px;
+        height: ${2 * radius}px;
+      }
+
+      .circle {
+        stroke: #0095b3;
+        stroke-dasharray: ${circumference} ${circumference};
+        stroke-dashoffset: ${(1 - progress / 100) * circumference};
+        fill: transparent;
+        stroke-width: ${stroke};
+
+        transition: stroke-dashoffset 0.5s;
+        transform: rotate(-90deg);
+        transform-origin: 50% 50%;
+      }
+    `;
+
+    this.circleElement.setAttribute('r', normalizedRadius);
+    this.circleElement.setAttribute('cx', radius);
+    this.circleElement.setAttribute('cy', radius);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    const newValueNumber = validateNumber(newValue);
+
+    // check if attribute value is number
+    if (newValueNumber === null) return;
+
+    switch (name) {
+      case 'radius':
+        if (newValueNumber < 0) return;
+        break;
+      case 'stroke':
+        if (newValueNumber < 0) return;
+        break;
+      case 'progress':
+        if (name === 'progress')
+          if (newValueNumber < 0 || newValueNumber > 100) return;
+        break;
+      default:
+        return;
+    }
+
+    this[`_${name}`] = newValueNumber;
+    this.updateComponent(this._radius, this._stroke, this._progress);
+  }
+
+  get radius() {
+    return this._radius;
+  }
+
+  set radius(val) {
+    const num = validateNumber(val);
+    this.setAttribute('radius', num);
+  }
+
+  get stroke() {
+    return this._stroke;
+  }
+
+  set stroke(val) {
+    const num = validateNumber(val);
+    this.setAttribute('stroke', num);
+  }
+
+  get progress() {
+    return this._progress;
+  }
+
+  set progress(val) {
+    const num = validateNumber(val);
+    this.setAttribute('progress', num);
   }
 }
 
