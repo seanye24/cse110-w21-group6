7abab9f3b6a60a3552ae63d474a04cb82f83acb6
Sean Ye <seanye24@gmail.com>
2021-02-24 15:39:44 -0800

    Add progress ring script

---
+++ src/scripts/progress-ring.js
@@ -0,0 +1,37 @@
+import ProgressRing from '../components/progress-ring';
+import { createElement } from '../utils';
+
+window.customElements.define('progress-ring', ProgressRing);
+let progressRingElement;
+
+const initializeProgressRing = (
+  container,
+  radius = 0,
+  stroke = 0,
+  progress = 0,
+) => {
+  progressRingElement = createElement('progress-ring', {
+    radius,
+    stroke,
+    progress,
+  });
+  container.appendChild(progressRingElement);
+};
+
+const setProgress = (progress) => {
+  progressRingElement.progress = progress;
+};
+
+/**
+ * Start the progress bar
+ * @param {number} tickLength - amount of progress to decrease
+ * @param {number} tickFrequency - how often to decrease progress (in ms)
+ */
+const startRing = (tickLength, tickFrequency) => {
+  const ticker = setInterval(() => {
+    if (progressRingElement.progress <= 0) clearInterval(ticker);
+    setProgress(progressRingElement.progress - tickLength);
+  }, tickFrequency);
+};
+
+export { initializeProgressRing, setProgress, startRing };
