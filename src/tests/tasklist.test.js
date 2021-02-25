d0d6b8f1a6c1f4e399fc0dc01de7150f77eb0458
Sean Ye <seanye24@gmail.com>
2021-02-24 22:05:49 -0800

    Update tasklist tests

--- src/tests/tasklist.test.js
+++ src/tests/tasklist.test.js
@@ -1,42 +1,41 @@
 // testing tasklist, crud operations
 
-const {
+import {
   initializeTaskList,
   addTask,
   getTasks,
   updateTask,
   deleteTask,
-} = require('../scripts/taskList');
+} from '../scripts/taskList';
 
-let tasks = [{ name: 'task1' }, { name: 'task2' }];
-window.localStorage.setItem('tasks', JSON.stringify(tasks));
+describe('testing tasklist', () => {
+  const tasks = [{ name: 'task1' }, { name: 'task2' }];
+  beforeEach(() => {
+    window.localStorage.clear();
+    window.localStorage.setItem('tasks', JSON.stringify(tasks));
+    initializeTaskList(document.body);
+  });
+  test('retrieves tasks from localStorage', () => {
+    expect(getTasks()).toStrictEqual(tasks);
+  });
 
-test('retrieves tasks from localStorage', () => {
-  initializeTaskList(document.body);
-  expect(getTasks()).toStrictEqual(tasks);
-});
+  test('add new task', () => {
+    const newTask = { title: 'task3', description: 'description3' };
+    addTask(newTask);
+    expect(getTasks()).toStrictEqual([...tasks, newTask]);
+  });
 
-test('add new task', () => {
-  const newTask = { title: 'task3', description: 'description3' };
-  addTask(newTask);
-  tasks.push(newTask);
-  expect(getTasks()).toStrictEqual(tasks);
-});
+  test('update existing task', () => {
+    const newTask = {
+      title: 'task2modified',
+      description: 'description2modified',
+    };
+    updateTask(tasks[1], newTask);
+    expect(getTasks()).toStrictEqual([tasks[0], newTask]);
+  });
 
-test('update existing task', () => {
-  const existingTask = tasks[1];
-  const newTask = {
-    title: 'task2modified',
-    description: 'description2modified',
-  };
-  updateTask(existingTask, newTask);
-  tasks[1] = newTask;
-  expect(getTasks()).toStrictEqual(tasks);
-});
-
-test('delete existing task', () => {
-  const existingTask = tasks[1];
-  deleteTask(existingTask);
-  tasks = [tasks[0], tasks[2]];
-  expect(getTasks()).toStrictEqual(tasks);
+  test('delete existing task', () => {
+    deleteTask(tasks[1]);
+    expect(getTasks()).toStrictEqual([tasks[0]]);
+  });
 });
