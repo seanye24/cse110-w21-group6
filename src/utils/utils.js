21659adc52222a5b8c77ef8bd1a28a6b7de716b9
Sean Ye <seanye24@gmail.com>
2021-03-02 23:01:30 -0800

    Update utils file

---
+++ src/utils/utils.js
@@ -0,0 +1,87 @@
+import {
+  DEFAULT_POMODORO_INTERVAL,
+  DEFAULT_SHORT_BREAK_INTERVAL,
+  DEFAULT_LONG_BREAK_INTERVAL,
+} from './constants';
+
+/**
+ * Creates an HTMLElement and set its attributes
+ * Created to reduce boilerplate from element creation
+ * @param {string} elementType - element tag name
+ * @param {{key: string}} props - element's attributes/properties
+ * @param {{option: string}} options - element options such as namespace
+ * @return element - new HTMLElement created
+ */
+export const createElement = (elementType, props = {}, options = {}) => {
+  const { namespace } = options;
+  let element;
+  if (namespace) element = document.createElementNS(namespace, elementType);
+  else element = document.createElement(elementType); // create element
+
+  // set attributes/properties
+  Object.entries(props).forEach(([key, value]) => {
+    if (namespace || !(key in element)) element.setAttribute(key, value);
+    else element[key] = value;
+  });
+
+  return element;
+};
+
+/**
+ * Tries to convert input to a number
+ * @param {any} value - to be converted to number
+ * @return {number | null} - number if successful, null otherwise
+ */
+export const validateNumber = (value) => {
+  const num = parseInt(value, 10);
+  return Number.isNaN(num) ? null : num;
+};
+
+/**
+ * Use promises to tick by specified tickLength
+ * @param {number} duration - duration of tick (in seconds)
+ * @return {Promise<void>} - promise that resolves after tick duration
+ */
+export const tick = async (duration) =>
+  new Promise((res) => setTimeout(res, 1000 * duration));
+
+/**
+ * Converts seconds into MM : SS
+ * @param {string} seconds - seconds to convert
+ * @return {[number, number]} - [minutes, seconds]
+ */
+export const getMinutesAndSeconds = (seconds) => {
+  return [Math.floor(seconds / 60), Math.floor(seconds % 60)];
+};
+
+/**
+ * Set text of announcement element
+ * @param {HTMLElement} element - announcement element
+ * @param {string} str - announcement to set
+ */
+export const setAnnouncement = (element, str) => {
+  element.innerText = str;
+};
+
+/**
+ * Initialize interval lengths, retrieve from localStorage if possible
+ * @return {pomodoroLength: number, shortBreakLength: number, longBreakLength: number} - lengths of intervals
+ */
+export const initializeIntervalLengths = () => {
+  let pomodoroLength = window.localStorage.getItem('pomodoroLength');
+  let shortBreakLength = window.localStorage.getItem('shortBreakLength');
+  let longBreakLength = window.localStorage.getItem('longBreakLength');
+  if (!pomodoroLength) {
+    pomodoroLength = DEFAULT_POMODORO_INTERVAL;
+    window.localStorage.setItem('pomodoroLength', pomodoroLength);
+  }
+  if (!shortBreakLength) {
+    shortBreakLength = DEFAULT_SHORT_BREAK_INTERVAL;
+    window.localStorage.setItem('shortBreakLength', shortBreakLength);
+  }
+  if (!longBreakLength) {
+    longBreakLength = DEFAULT_LONG_BREAK_INTERVAL;
+    window.localStorage.setItem('longBreakLength', longBreakLength);
+  }
+  return { pomodoroLength, shortBreakLength, longBreakLength };
+};
