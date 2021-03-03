26a981c284db423603d2cb0a72420f04923bec18
Sean Ye <seanye24@gmail.com>
2021-03-03 01:43:18 -0800

    Add Timer component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as TaskItem } from './TaskItem';
 export { default as TaskList } from './TaskList';
 export { default as TaskItemForm } from './TaskItemForm';
+export { default as Timer } from './Timer';
 export { default as ProgressRing } from './ProgressRing';
