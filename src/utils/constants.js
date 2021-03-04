cd53fd9797ec9fb7532347b307a12d85f101efb5
Sean Ye <seanye24@gmail.com>
2021-03-04 00:31:56 -0800

    Update announcement messages

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,9 +8,9 @@ export const DEFAULT_SHORT_BREAK_INTERVAL = 5 * 60;
 export const DEFAULT_LONG_BREAK_INTERVAL = 15 * 60;
 
 // announement messages
-export const POMODORO_ANNOUNCEMENT = 'Currently in a pomodoro.';
-export const SHORT_BREAK_ANNOUNCEMENT = 'Currently in a short break.';
-export const LONG_BREAK_ANNOUNCEMENT = 'Currently in a long break.';
+export const POMODORO_ANNOUNCEMENT = 'Focus.';
+export const SHORT_BREAK_ANNOUNCEMENT = 'Enjoy your short break!';
+export const LONG_BREAK_ANNOUNCEMENT = 'Good work. Enjoy a longer break!';
 export const INTRO_ANNOUNCEMENT = 'What do you need to work on today?';
 export const INTRO_SELECT_TASK_ANNOUNCEMENT = 'Select a task then hit start!';
 export const NEW_SELECT_TASK_ANNOUNCEMENT = 'Select another task.';
@@ -18,6 +18,7 @@ export const CONTINUING_SELECT_TASK_ANNOUNCEMENT =
   'Select another task if you would like.';
 export const TASK_COMPLETION_QUESTION = 'Did you finish the task?';
 export const END_OF_SESSION_ANNOUNCEMENT = 'Good work today!';
+export const NO_TASKS_ANNOUNCEMENT = 'No work to be done.';
 
 // interval names
 export const POMODORO_INTERVAL = 'pomodoroInterval';
