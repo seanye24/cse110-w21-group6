1d41b51e9739da88f53be2022cc7ec738a63b49d
Sean Ye <seanye24@gmail.com>
2021-03-10 14:53:38 -0800

    Update docs

--- src/scripts/pomodoroCircles.js
+++ src/scripts/pomodoroCircles.js
@@ -6,20 +6,20 @@ let circlesElement;
 
 /**
  * Get number of filled pomodoro circles
- * @return {number} - amount of filled pomodoro circles
+ * @return {number} - number of filled pomodoro circles
  */
 const getCircleCount = () => circlesElement.circleCount;
 
 /**
  * Set number of filled pomodoro circles
- * @param {number} progress - amount of filled pomodoro circles to set
+ * @param {number} circleCount - number of filled pomodoro circles to set
  */
 const setCircleCount = (circleCount) => {
   circlesElement.circleCount = circleCount;
 };
 
 /**
- * Set pomodoro circles
+ * Initialize pomodoro circles
  * @param {HTMLElement} element - pomodoro circles element
  */
 const initializePomodoroCircles = (element) => {
