52b46e5f7769887f1f14dd09a3d698a540fa9f65
Sean Ye <seanye24@gmail.com>
2021-03-11 15:42:28 -0800

    Add progress ring validation

--- src/scripts/progressRing.js
+++ src/scripts/progressRing.js
@@ -2,6 +2,8 @@
  * @file Manage progress ring for page
  */
 
+import { validateLength, validateProgress } from '../utils/progressRing';
+
 let progressRingElement;
 
 /**
@@ -13,6 +15,44 @@ const initializeProgressRing = (element) => {
 };
 
 /**
+ * Get radius of ring
+ * @return {number} - radius of progress ring
+ */
+const getRadius = () => progressRingElement.radius;
+
+/**
+ * Set radius of ring
+ * @param {number} radius - new radius to set
+ */
+const setRadius = (value) => {
+  const radius = validateLength(value);
+  if (radius === null) {
+    return;
+  }
+
+  progressRingElement.radius = radius;
+};
+
+/**
+ * Get stroke of ring
+ * @return {number} - stroke of progress ring
+ */
+const getStroke = () => progressRingElement.stroke;
+
+/**
+ * Set stroke of ring
+ * @param {number} value - new stroke to set
+ */
+const setStroke = (value) => {
+  const stroke = validateLength(value);
+  if (stroke === null) {
+    return;
+  }
+
+  progressRingElement.stroke = stroke;
+};
+
+/**
  * Get progress
  * @return {number} - current progress
  */
@@ -20,38 +60,23 @@ const getProgress = () => progressRingElement.progress;
 
 /**
  * Set progress
- * @param {number} progress - progress to set
+ * @param {number} value - progress to set
  */
-const setProgress = (progress) => {
-  progressRingElement.progress = JSON.stringify(progress);
+const setProgress = (value) => {
+  const progress = validateProgress(value);
+  if (progress === null) {
+    return;
+  }
+
+  progressRingElement.progress = progress;
 };
-
-/**
- * Set radius of ring
- * @param {number} radius - new radius to set
- */
-const setRadiusStroke = (radius, stroke) => {
-  progressRingElement.radius = radius;
-  progressRingElement.stroke = stroke;
-};
-
-/**
- * Get radius of ring
- * @return {number} radius
- */
-const getRadius = () => progressRingElement.radius;
-
-/**
- * Get stroke of ring
- * @return {number} stroke
- */
-const getStroke = () => progressRingElement.stroke;
 
 export {
   initializeProgressRing,
-  getProgress,
-  setProgress,
-  setRadiusStroke,
   getRadius,
   getStroke,
+  getProgress,
+  setRadius,
+  setStroke,
+  setProgress,
 };
