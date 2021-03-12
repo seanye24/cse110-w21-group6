eaccc037d5ba19a1b4da57d57a4fec23d41a42f3
Fernando Bracamonte <fernandobrac@hotmail.com>
2021-03-11 19:10:27 -0800

    completed confirmationPopup test

---
+++ src/tests/confirmationPopup.test.js
@@ -0,0 +1,70 @@
+import {
+    initializePopup,
+    openPopup,
+    closePopup,
+} from '../scripts/confirmationPopup'
+import { createElement } from "../utils/helpers";
+
+describe('testing popup functionality', () => {
+    
+    let confirmationOverlay;
+    let confirmationPopup;
+    let noButton;
+    let yesButton;
+
+    beforeEach(() => {
+        let popupElement = createElement('div', {
+            innerHTML: `
+            <div id="confirmation-overlay" class="overlay">
+                <div id="confirmation-popup" class="popup">
+                    <p class="confirmation-question">
+                        Are you sure you want to end the session?
+                    </p>
+                    <div class="confirmation-button-container">
+                        <button class="confirmation-button confirmation-no-button">
+                            Cancel
+                        </button>
+                        <button class="confirmation-button confirmation-yes-button">
+                            Confirm
+                        </button>
+                    </div>
+                </div>
+            </div>
+            `
+        });
+    
+        confirmationOverlay = popupElement.querySelector('#confirmation-overlay');
+        confirmationPopup = popupElement.querySelector('#confirmation-popup');
+        noButton = popupElement.querySelector('.confirmation-no-button');
+        yesButton = popupElement.querySelector('.confirmation-yes-button');
+    });
+
+    test('popup opens correctly', () => {
+        initializePopup(confirmationOverlay);
+        openPopup();
+        expect(confirmationOverlay.classList.contains('active')).toBe(true);
+    });
+
+    test('popup closes correctly', () => {
+        initializePopup(confirmationOverlay);
+        closePopup();
+        expect(confirmationOverlay.classList.contains('active')).toBe(false);
+    });
+
+    test('button click closes popop', () => {
+        const mockCallback = jest.fn();
+
+        initializePopup(confirmationOverlay, mockCallback);
+        openPopup();
+        expect(confirmationOverlay.classList.contains('active')).toBe(true);
+        yesButton.click();
+        expect(confirmationOverlay.classList.contains('active')).toBe(false);
+        expect(mockCallback).toHaveBeenCalled();
+
+        openPopup();
+        expect(confirmationOverlay.classList.contains('active')).toBe(true);
+        noButton.click();
+        expect(confirmationOverlay.classList.contains('active')).toBe(false);
+        expect(mockCallback).toHaveBeenCalled();
+    });
+});
