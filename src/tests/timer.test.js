286003ed4f1a34f0c1d8193674754586e1dcf70f
Fernando Bracamonte <fernandobrac@hotmail.com>
2021-03-14 19:48:25 -0700

    refactored unit tests

--- src/tests/timer.test.js
+++ src/tests/timer.test.js
@@ -1,221 +1,244 @@
 import { Timer } from '../components';
 import { createElement } from '../utils/helpers';
 import { initializeTimer, setTimer, getTime } from '../scripts/timer';
+import { validateTime, validateContainerRadius } from '../utils/timer';
 
 customElements.define('timer-component', Timer);
 
+describe('testing timer utils', () => {
+  test('validateTime returns time on valid inputs', () => {
+    // set array to largest value of 3599
+    const time = new Array(3600).fill(null).map((e, i) => i);
+    time.forEach((value) => {
+      expect(validateTime(value)).toBe(value);
+    });
+  });
+
+  test('validateTime returns null on invalid inputs', () => {
+    const invalidTimes = [
+      '$%^#^$',
+      'hello',
+      null,
+      undefined,
+      NaN,
+      {},
+      -1,
+      -10,
+      3600,
+      5645646,
+      true,
+      false,
+    ];
+    invalidTimes.forEach((value) => {
+      expect(validateTime(value)).toBe(null);
+    });
+  });
+
+  test('validateContainerRadius returns container radius on valid inputs', () => {
+    const radius = new Array(100).fill(null).map((e, i) => i);
+    radius.forEach((value) => {
+      expect(validateContainerRadius(value)).toBe(value);
+    });
+  });
+
+  test('validateContainerRadius returns null on invalid inputs', () => {
+    const invalidRadius = [
+      '$%^#^$',
+      'hello',
+      null,
+      undefined,
+      NaN,
+      {},
+      -1,
+      -10,
+      true,
+      false,
+    ];
+    invalidRadius.forEach((value) => {
+      expect(validateContainerRadius(value)).toBe(null);
+    });
+  });
+});
+
 describe('testing setTimer', () => {
-  test('setting valid inputs are saved', () => {
-    const timerElement = createElement('timer-component', {
+  let timerElement;
+
+  beforeEach(() => {
+    timerElement = createElement('timer-component', {
       time: 1500,
       containerRadius: 10,
       className: 'timer',
     });
-    initializeTimer(timerElement);
 
-    setTimer(0);
-    expect(timerElement.time).toBe(0);
-    setTimer(25);
-    expect(timerElement.time).toBe(25);
-    setTimer(60);
-    expect(timerElement.time).toBe(60);
-    setTimer(1000);
-    expect(timerElement.time).toBe(1000);
-    setTimer('7');
-    expect(timerElement.time).toBe(7);
-    setTimer(1499);
-    expect(timerElement.time).toBe(1499);
+    initializeTimer(timerElement);
   });
 
-  test('invalid inputs are not saved', () => {
-    const timerElement = createElement('timer-component', {
-      time: 1500,
-      containerRadius: 10,
-      className: 'timer',
-    });
-    initializeTimer(timerElement);
+  test('get attribute', () => {
+    expect(timerElement.getAttribute('time')).toBe('1500');
+    expect(timerElement.getAttribute('container-radius')).toBe('10');
+  });
 
-    setTimer(-100);
+  test('if input valid, set attribute changes attribute and property', () => {
+    const values = new Array(3600).fill(null).map((e, i) => i);
+    values.forEach((value) => {
+      timerElement.setAttribute('time', value);
+      expect(timerElement.getAttribute('time')).toBe(`${value}`);
+      expect(timerElement.time).toBe(value);
+
+      timerElement.setAttribute('container-radius', value);
+      expect(timerElement.getAttribute('container-radius')).toBe(`${value}`);
+      expect(timerElement.containerRadius).toBe(value);
+    });
+  });
+
+  test("if input invalid, set attribute doesn't change attribute and property", () => {
+    const invalidTimes = [
+      '$%^#^$',
+      'hello',
+      null,
+      undefined,
+      NaN,
+      {},
+      -1,
+      -10,
+      3600,
+      5645646,
+      true,
+      false,
+    ];
+    const invalidRadius = [
+      '$%^#^$',
+      'hello',
+      null,
+      undefined,
+      NaN,
+      {},
+      -1,
+      -10,
+      true,
+      false,
+    ];
+
+    invalidTimes.forEach((value) => {
+      timerElement.setAttribute('time', value);
+      expect(timerElement.getAttribute('time')).toBe('1500');
+      expect(timerElement.time).toBe(1500);
+    });
+
+    invalidRadius.forEach((value) => {
+      timerElement.setAttribute('container-radius', value);
+      expect(timerElement.getAttribute('container-radius')).toBe('10');
+      expect(timerElement.containerRadius).toBe(10);
+    });
+  });
+
+  test('getter functions', () => {
     expect(timerElement.time).toBe(1500);
-    setTimer(3600);
-    expect(timerElement.time).toBe(1500);
-    setTimer(1000000);
-    expect(timerElement.time).toBe(1500);
-    setTimer('#%$%$#%');
-    expect(timerElement.time).toBe(1500);
-    setTimer('hello');
-    expect(timerElement.time).toBe(1500);
-    setTimer(NaN);
-    expect(timerElement.time).toBe(1500);
+    expect(timerElement.containerRadius).toBe(10);
+  });
+
+  test('if input valid, setter function changes attribute and property', () => {
+    const values = new Array(3600).fill(null).map((e, i) => i);
+    values.forEach((value) => {
+      timerElement.time = value;
+      expect(timerElement.getAttribute('time')).toBe(`${value}`);
+      expect(timerElement.time).toBe(value);
+
+      timerElement.containerRadius = value;
+      expect(timerElement.getAttribute('container-radius')).toBe(`${value}`);
+      expect(timerElement.containerRadius).toBe(value);
+    });
+  });
+
+  test("if input invalid, setter function doesn't change attribute and property", () => {
+    const invalidTimes = [
+      '$%^#^$',
+      null,
+      undefined,
+      NaN,
+      {},
+      -1,
+      -10,
+      3600,
+      5645646,
+      true,
+      false,
+    ];
+    const invalidRadius = [
+      '$%^#^$',
+      null,
+      undefined,
+      NaN,
+      {},
+      -1,
+      -10,
+      true,
+      false,
+    ];
+
+    invalidTimes.forEach((value) => {
+      timerElement.time = value;
+      expect(timerElement.getAttribute('time')).toBe('1500');
+      expect(timerElement.time).toBe(1500);
+    });
+
+    invalidRadius.forEach((value) => {
+      timerElement.containerRadius = value;
+      expect(timerElement.getAttribute('container-radius')).toBe('10');
+      expect(timerElement.containerRadius).toBe(10);
+    });
   });
 });
 
-describe('testing getTime', () => {
-  test('setting valid inputs are saved', () => {
-    const timerElement = createElement('timer-component', {
+describe('testing timer script', () => {
+  let timerElement;
+
+  beforeEach(() => {
+    timerElement = createElement('timer-component', {
       time: 1500,
       containerRadius: 10,
       className: 'timer',
     });
-    initializeTimer(timerElement);
 
-    timerElement.time = 0;
-    expect(getTime()).toBe(0);
-    timerElement.time = 25;
-    expect(getTime()).toBe(25);
-    timerElement.time = 60;
-    expect(getTime()).toBe(60);
-    timerElement.time = 1000;
-    expect(getTime()).toBe(1000);
-    timerElement.time = '7';
-    expect(getTime()).toBe(7);
-    timerElement.time = 1499;
-    expect(getTime()).toBe(1499);
+    initializeTimer(timerElement);
   });
 
-  test('invalid inputs are not saved', () => {
-    const timerElement = createElement('timer-component', {
-      time: 1500,
-      containerRadius: 10,
-      className: 'timer',
-    });
-    initializeTimer(timerElement);
-
-    timerElement.time = -100;
+  test('get function', () => {
+    // initial value
     expect(getTime()).toBe(1500);
-    timerElement.time = 3600;
-    expect(getTime()).toBe(1500);
-    timerElement.time = 1000000;
-    expect(getTime()).toBe(1500);
-    timerElement.time = '#%$%$#%';
-    expect(getTime()).toBe(1500);
-    timerElement.time = 'hello';
-    expect(getTime()).toBe(1500);
-    timerElement.time = NaN;
-    expect(getTime()).toBe(1500);
-  });
-});
 
-describe('testing getter/setter functions of timer component', () => {
-  test('valid time inputs are saved', () => {
-    const timerElement = createElement('timer-component', {
-      time: 1500,
-      containerRadius: 10,
-      className: 'timer',
+    const values = new Array(3600).fill(null).map((e, i) => i);
+    values.forEach((value) => {
+      timerElement.time = value;
+      expect(getTime()).toBe(value);
     });
-
-    initializeTimer(timerElement);
-
-    timerElement.time = 1;
-    expect(timerElement.time).toBe(1);
-    timerElement.time = 0;
-    expect(timerElement.time).toBe(0);
-    timerElement.time = 15;
-    expect(timerElement.time).toBe(15);
-    timerElement.time = 1999;
-    expect(timerElement.time).toBe(1999);
-    timerElement.time = 3599;
-    expect(timerElement.time).toBe(3599);
   });
 
-  test('invalid time inputs are not saved', () => {
-    const timerElement = createElement('timer-component', {
-      time: 1500,
-      containerRadius: 10,
-      className: 'timer',
+  test('if input valid, set function changes property', () => {
+    const values = new Array(3600).fill(null).map((e, i) => i);
+    values.forEach((value) => {
+      setTimer(value);
+      expect(timerElement.time).toBe(value);
     });
-
-    initializeTimer(timerElement);
-
-    timerElement.time = 'gferg';
-    expect(timerElement.time).toBe(1500);
-    timerElement.time = '#%@$%^';
-    expect(timerElement.time).toBe(1500);
-    timerElement.time = NaN;
-    expect(timerElement.time).toBe(1500);
-    timerElement.time = -1999;
-    expect(timerElement.time).toBe(1500);
-    timerElement.time = 1000000;
-    expect(timerElement.time).toBe(1500);
-    timerElement.time = null;
-    expect(timerElement.time).toBe(1500);
   });
 
-  test('valid container radius inputs are saved', () => {
-    const timerElement = createElement('timer-component', {
-      time: 1500,
-      containerRadius: 10,
-      className: 'timer',
+  test('if input invalid, set function does not change property', () => {
+    const invalidTimes = [
+      '$%^#^$',
+      null,
+      undefined,
+      NaN,
+      {},
+      -1,
+      -10,
+      3600,
+      5645646,
+      true,
+      false,
+    ];
+    invalidTimes.forEach((value) => {
+      setTimer(value);
+      expect(timerElement.time).toBe(1500);
     });
-
-    initializeTimer(timerElement);
-
-    timerElement.containerRadius = 1;
-    expect(timerElement.containerRadius).toBe(1);
-    timerElement.containerRadius = 0;
-    expect(timerElement.containerRadius).toBe(0);
-    timerElement.containerRadius = 159;
-    expect(timerElement.containerRadius).toBe(159);
-  });
-
-  test('invalid container radius inputs are not saved', () => {
-    const timerElement = createElement('timer-component', {
-      time: 1500,
-      containerRadius: 10,
-      className: 'timer',
-    });
-
-    initializeTimer(timerElement);
-
-    timerElement.containerRadius = 'gferg';
-    expect(timerElement.containerRadius).toBe(10);
-    timerElement.containerRadius = '#%@$%^';
-    expect(timerElement.containerRadius).toBe(10);
-    timerElement.containerRadius = NaN;
-    expect(timerElement.containerRadius).toBe(10);
-    timerElement.containerRadius = -1999;
-    expect(timerElement.containerRadius).toBe(10);
-    timerElement.containerRadius = null;
-    expect(timerElement.containerRadius).toBe(10);
-  });
-});
-
-describe('testing time and containerRadius attributes of timer component', () => {
-  test('valid time inputs are saved', () => {
-    const timerElement = createElement('timer-component', {
-      time: 1500,
-      containerRadius: 10,
-      className: 'timer',
-    });
-
-    initializeTimer(timerElement);
-
-    timerElement.setAttribute('time', 1);
-    expect(timerElement.getAttribute('time')).toBe('1');
-    timerElement.setAttribute('time', 0);
-    expect(timerElement.getAttribute('time')).toBe('0');
-    timerElement.setAttribute('time', 15);
-    expect(timerElement.getAttribute('time')).toBe('15');
-    timerElement.setAttribute('time', 1999);
-    expect(timerElement.getAttribute('time')).toBe('1999');
-    timerElement.setAttribute('time', 3599);
-    expect(timerElement.getAttribute('time')).toBe('3599');
-  });
-
-  test('valid container radius inputs are saved', () => {
-    const timerElement = createElement('timer-component', {
-      time: 1500,
-      containerRadius: 10,
-      className: 'timer',
-    });
-
-    initializeTimer(timerElement);
-
-    timerElement.setAttribute('containerRadius', 1);
-    expect(timerElement.getAttribute('containerRadius')).toBe('1');
-    timerElement.setAttribute('containerRadius', 0);
-    expect(timerElement.getAttribute('containerRadius')).toBe('0');
-    timerElement.setAttribute('containerRadius', 159);
-    expect(timerElement.getAttribute('containerRadius')).toBe('159');
   });
 });
